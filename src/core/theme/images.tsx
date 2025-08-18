542a2892b11dbd2c42ba4cb4b18e0fdb6ae03f39
Lisandro Rodriguez Prados <lisandro.rodriguez@consultr.net>
2025-08-17 22:26:41 -0300

    nuevo build

--- src/core/theme/images.tsx
+++ src/core/theme/images.tsx
@@ -1,13 +1,13 @@
-import logo from '../../../public/logo.png'
-import green1 from '../../../public/images/green1.jpg'
-import green2 from '../../../public/images/green2.jpg'
-import green3 from '../../../public/images/green3.jpg'
-import rezum from '../../../public/images/rezum.jpg'
-import rezum2 from '../../../public/images/rezum2.jpg'
-import rezum3 from '../../../public/images/rezum3.jpg'
-import toscas1 from '../../../public/images/toscas1.jpg'
-import toscas2 from '../../../public/images/toscas2.jpg'
-import toscas3 from '../../../public/images/toscas3.jpg'
+import logo from '/logo.png'
+import green1 from '/images/green1.jpg'
+import green2 from '/images/green2.jpg'
+import green3 from '/images/green3.jpg'
+import rezum from '/images/rezum.jpg'
+import rezum2 from '/images/rezum2.jpg'
+import rezum3 from '/images/rezum3.jpg'
+import toscas1 from '/images/toscas1.jpg'
+import toscas2 from '/images/toscas2.jpg'
+import toscas3 from '/images/toscas3.jpg'
 
 export const images = {
   logo,
